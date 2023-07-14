9b8105c01a7c16fb7ef9e1105bc36d7f9dfbb453
Sahil Singh Tomar <sahilsinghtomar14435@gmail.com>
2023-07-14 17:22:14 +0530

    added model to titanic.py

--- titanic.py
+++ titanic.py
@@ -1,5 +1,29 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pickle
+from joblib import dump, load
+
 
 st.title("Titanic Dataset")
+
+pClass = st.number_input("Enter Pclass (1, 2, 3)")
+sex = st.radio("Female/Male", [0, 1])
+age = st.number_input("Enter Age")
+sibsp = st.number_input("Enter Sibsp (0, 1, 2, 3)")
+parch = st.number_input("Enter Parch (0, 1, 2)")
+fare = st.number_input("Enter Fare")
+
+# https://titanic-prediction.streamlit.app/
+
+clicked = st.button("Get Prediction")
+
+if clicked:
+    data = [pClass, sex, age, parch, fare]
+
+    model = load("model.joblib")
+
+    if model.predict(data) == 0:
+        print("Not Survived")
+    else:
+        print("Survived")
